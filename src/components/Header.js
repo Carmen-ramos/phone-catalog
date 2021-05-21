f0c0800c1f6e887c998510923139a7a3af09e044
Carmen Ramos <carmen.rama91@gmail.com>
2021-05-21 21:38:05 +0200

    add mobile header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
-import logo from "../images/soamee.png";
-// import "../stylesheets/Header.scss";
+import logo from "../images/soamelogo.png";
+import "../stylesheets/Header.scss";
 
 function Header() {
   return (
     <header className="header">
       <img className="header__img" src={logo} alt="Soamee logo image" />
-      <h2>Phone Catalog</h2>
+      <h2 className="header__title">Phone Catalog</h2>
     </header>
   );
 }
